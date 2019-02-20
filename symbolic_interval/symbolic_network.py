3747302a8e4b9418d71e648f1417a81b6ee45cd8
Shiqi Wang <tcwangshiqi@cs.columbia.edu>
2019-02-19 23:22:01 -0500

    detach

--- symbolic_interval/symbolic_network.py
+++ symbolic_interval/symbolic_network.py
@@ -153,20 +153,12 @@ class Interval_ReLU(nn.Module):
 			mask = appr_condition*((upper)/(upper-lower+0.000001))
 			mask = mask + 1 - appr_condition
 			mask = mask.detach()
-			'''
+			
 			if(ix.use_cuda):
 				mask = mask*((upper>0).type(torch.Tensor).\
 							cuda(device=ix.c.get_device()))
 			else:
 				mask = mask*(upper>0).type(torch.Tensor)
-			mask = mask.detach()
-			'''
-			mask = appr_condition*((upper)/(upper-lower+0.000001))
-			if(ix.use_cuda):
-				mask += (((upper>0)*(lower>0)).type(torch.Tensor).\
-							cuda(device=ix.c.get_device()))
-			else:
-				mask += ((upper>0)*(lower>0)).type(torch.Tensor)
 			mask = mask.detach()
 			
 			m = int(appr_condition.sum())
